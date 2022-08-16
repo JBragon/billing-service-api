cd55050528994df20b59e1be45094692b9321f16
Jordan Bragon <jordan.bragon@alliedbrasil.com.br>
2022-08-16 20:41:09 -0300

    Teste de unidade implementado

---
+++ BillingService.Unit.Test/BillingConsumerTest.cs
@@ -0,0 +1,40 @@
+﻿using Business.Interfaces;
+using Models.Infrastructure;
+using Moq;
+using Xunit;
+
+namespace BillingConsumerService.Unit.Test
+{
+    public class BillingConsumerTest
+    {
+        private readonly Mock<IBillingConsumerService> _billingConsumerService;
+
+        public BillingConsumerTest()
+        {
+            var fixture = new TestFixture();
+            _billingConsumerService = fixture._billingConsumerService;
+        }
+
+        [Fact]
+        public async Task BillingConsumerService_Calculate_Success()
+        {
+
+            var result = await _billingConsumerService.Object.Calculate();
+
+            Assert.True(result is not null);
+            Assert.True(result.successList.Any());
+            Assert.IsType<CalculateResponse>(result);
+        }
+
+        [Fact]
+        public async Task BillingConsumerService_Calculate_Error()
+        {
+
+            var result = await _billingConsumerService.Object.Calculate();
+
+            Assert.True(result is not null);
+            Assert.True(result.errorList.Any());
+            Assert.IsType<CalculateResponse>(result);
+        }
+    }
+}
---
+++ BillingService.Unit.Test/TestFixture.cs
@@ -0,0 +1,46 @@
+﻿using Business.Interfaces;
+using Models.HttpResponse;
+using Models.Infrastructure;
+using Moq;
+using Moq.AutoMock;
+
+namespace BillingConsumerService.Unit.Test
+{
+    public class TestFixture
+    {
+
+        public readonly Mock<IBillingConsumerService> _billingConsumerService;
+
+        public TestFixture()
+        {
+            AutoMocker mocker = new AutoMocker();
+
+            CalculateResponse response = new CalculateResponse
+            {
+                successList = new List<BillingResponse>
+                {
+                    new BillingResponse {
+                        Id = Guid.NewGuid().ToString(),
+                        CPF = "456.462.300-12",
+                        DueDate = DateTime.Now,
+                        ChargeAmount = 1234
+                    }
+                },
+                errorList = new List<BillingResponseError>
+                {
+                    new BillingResponseError
+                    {
+                        Error ="CPF Inválido",
+                        CPF = "456.462.300-18",
+                        DueDate = DateTime.Now,
+                        ChargeAmount = 1234
+                    }
+                }
+            };
+
+            mocker.GetMock<IBillingConsumerService>().Setup(uow => uow.Calculate(It.IsAny<int>())).Returns(Task.FromResult(response));
+
+            _billingConsumerService = mocker.GetMock<IBillingConsumerService>();
+        }
+    }
+}
